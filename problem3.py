b786fbc55d8dd5cca2f4d8d935b6fd0013d7ac2c
Lukáš Hlubuček <lukas.hlubucek@gmail.com>
2016-07-23 00:55:58 +0200

    Problem 3

---
+++ problem3.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+"""
+Largest prime factor
+Problem 3
+
+The prime factors of 13195 are 5, 7, 13 and 29.
+
+What is the largest prime factor of the number 600851475143 ?
+"""
+to_resolve = 600851475143
+ret = set()
+i = 1
+while to_resolve != 1:
+    i += 1
+    if to_resolve % i == 0:
+        to_resolve -= to_resolve / i
+        ret.add(i)
+        i = 1
+print sorted(ret)[len(ret) - 1]
