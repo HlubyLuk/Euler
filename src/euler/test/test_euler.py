9b3369d09758fe3f4d7237f23ee6e456b52e1379
Lukáš Hlubuček <lukas.hlubucek@gmail.com>
2018-11-03 22:21:19 +0100

    Run autopep8 and flake8.

--- src/euler/test/test_euler.py
+++ src/euler/test/test_euler.py
@@ -10,55 +10,49 @@ from euler.euler import Problem1, Problem2, Problem3, Problem4, Problem5,\
 
 class Test(unittest.TestCase):
 
-    def setUp(self):
-        pass
-
-    def tearDown(self):
-        pass
-
-    def testProblem1(self):
+    def test_problem1(self):
         self.assertEqual(233168, Problem1().solve())
 
-    def testProblem2(self):
+    def test_problem2(self):
         self.assertEqual(4613732, Problem2().solve())
 
-    def testProblem3(self):
+    def test_problem3(self):
         self.assertEqual(6857, Problem3().solve())
 
-    def testProblem4(self):
+    def test_problem4(self):
         self.assertEqual(906609, Problem4().solve())
 
-    def testProblem5(self):
+    def test_problem5(self):
         self.assertEqual(232792560, Problem5().solve())
 
-    def testProblem6(self):
+    def test_problem6(self):
         self.assertEqual(25164150, Problem6().solve())
 
-    def testProblem7(self):
+    def test_problem7(self):
         self.assertEqual(104743, Problem7().solve())
 
-    def testProblem8(self):
+    def test_problem8(self):
         self.assertEqual(23514624000, Problem8().solve())
 
     def testLcm(self):
         self.assertEqual(42, MockProblem().lcm([6, 21]))
 
-    def testPrimeFactors(self):
+    def test_primeFactors(self):
         self.assertEqual({2: 2, 3: 1, 5: 1}, MockProblem().prime_factors(60))
 
     def testIsPalindromic(self):
         self.assertTrue(MockProblem().is_palindromic(990099))
 
-    def testIsNotPalidromic(self):
+    def test_is_not_palidromic(self):
         self.assertFalse(MockProblem().is_palindromic(123456))
 
-    def testIsPrime2(self):
+    def test_is_prime2(self):
         self.assertTrue(MockProblem().is_prime(2))
 
-    def testIsPrime29(self):
+    def test_is_prime29(self):
         self.assertTrue(MockProblem().is_prime(29))
 
-    def testIsNotPrime(self):
+    def test_is_not_prime(self):
         self.assertFalse(MockProblem().is_prime(100))
 
 
