80ea5fa63ae142728697a866b4d20b0eb50f64de
Lukáš Hlubuček <lukas.hlubucek@gmail.com>
2019-01-27 22:50:43 +0100

    Problem 33 template.

--- src/euler/euler.py
+++ src/euler/euler.py
@@ -1450,6 +1450,28 @@ class Problem32(Problem):
         return sum(cache)
 
 
+class Problem33(Problem):
+    '''
+    Digit cancelling fractions
+    Problem 33
+    The fraction 49/98 is a curious fraction, as an inexperienced
+    mathematician in attempting to simplify it may incorrectly believe
+    that 49/98 = 4/8, which is correct, is obtained by cancelling the 9s.
+
+    We shall consider fractions like, 30/50 = 3/5, to be trivial examples.
+
+    There are exactly four non-trivial examples of this type of fraction,
+    less than one in value, and containing two digits in the numerator
+    and denominator.
+
+    If the product of these four fractions is given in its lowest common
+    terms, find the value of the denominator.
+    '''
+
+    def solve(self):
+        return 0
+
+
 if __name__ == '__main__':
     # import time
     # start = time.time()
@@ -1484,6 +1506,7 @@ if __name__ == '__main__':
     # Problem29().solve()
     # Problem30().solve()
     # Problem31().solve()
-    print(Problem32().solve())
+    # Problem32().solve()
+    print(Problem33().solve())
     # print(time.time() - start)
     pass
